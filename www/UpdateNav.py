d83bf099734a7ecbfce607b620b918c021bde087
Alexander <alex.c.hayes08@gmail.com>
2017-04-04 04:42:42 -0500

    Don't run the python script, i havent tested it yet

--- www/UpdateNav.py
+++ www/UpdateNav.py
@@ -2,7 +2,7 @@ from os import listdir
 from os.path import isfile, join
 import fileinput
 
-results = []
+updates = []
 
 # Get all files, except index.html, that end in .html
 filesToUpdate = []
@@ -10,7 +10,7 @@ for file in listdir():
     if isfile(file) and file.endswith(".html") and not file.startswith("index.html"):
         filesToUpdate.push(file)
 
-# Get the nav html and store in results
+# Get the nav html and store in updates
 with open('./index.html', 'r') as inputfile:
     foundNavStart = False
     for line in inputfile:
@@ -18,10 +18,28 @@ with open('./index.html', 'r') as inputfile:
             if line.startswith("<!--NAVEND-->"):
                 break;
             else:
-                results.push(line)
+                updates.push(line)
         elif line.startsWith("<!--NAVSTART-->"):
             foundNavStart = True
 
 # Then replace the html in filesToUpdate between the <!--NAVSTART--> 
 # and <!--NAVEND-->
-for file in filesToUpdate:    
+for file in filesToUpdate:
+    filelines = []
+    with open(file, 'r+') as inputfile:
+        navStart = False
+        for line in inputfile:
+            if navStart:
+                if line.startsWith("<!--NAVEND-->")
+                    navStart = False
+                continue
+            else
+                if line.startsWith("<!--NAVSTART-->")
+                    navStart = True
+                    filelines = filelines + updates
+                else
+                    filelines.push(line)
+
+        inputfile.truncate()
+        for line in filelines
+            file.write(line);
