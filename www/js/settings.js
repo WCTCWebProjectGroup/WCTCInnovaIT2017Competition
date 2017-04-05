3884be5ee9de805258d7c7828b4c01f929723bcf
alexander <alex.c.hayes08@gmail.com>
2017-04-05 16:52:43 -0500

    Google auth codes

--- www/js/settings.js
+++ www/js/settings.js
@@ -2,13 +2,44 @@ var settings = new function () {
     var _blackboardCookieName = "blackboardtoken";
     var _googleCookieName = "googletoken";
 
+    function Cookie (name, value, expiration) {
+        let _name = name;
+        let _value = value;
+        let _expiration = Date.parse(expiration);
+        this.Name = _name;
+        this.Value = _value;
+        this.Expiration = _expiration;
+
+        this.toString = function () {
+            return "\"" + _name + "\"=" + _value + "; expires=" + Date.parse(_expiration).toUTCString() + "; path=/";
+        }
+    }
+
+    function _getCookie (cookieName) {
+        var fullCookieName = cookieName + "=";
+        var decodedCookies = decodeURIComponent(document.cookie);
+        decodedCookies
+            .split(';')
+            .forEach(function(cookiePart) {
+                while (cookiePart.charAt(0) == " ") {
+                    cookiePart = cookiePart.substring(1);
+                }
+
+                if (cookiePart.indexOf(fullCookieName) == 0) {
+                    return cookiePart.substring(fullCookieName.length, cookiePart.length);
+                }
+            });
+
+        return "";
+    }
+
     function _applyTheme () {
         database
             .GetTheme()
             .then(function (themeObj) {
                 document
                     .querySelectorAll(".themeable")
-                    .foreach(function (themeable) {
+                    .forEach(function (themeable) {
                         themeable.setAttribute("class", "themeable " + themeObj);
                 });
             });
@@ -21,4 +52,24 @@ var settings = new function () {
         xhr.open("POST", "https://wctc.blackboard.com/webapps/login/");
         xhr.withCredentials = true;
     }
+
+    function _onSignIn(googleUser) {
+        var profile = googleUser.getBasicProfile();
+        console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+        console.log('Name: ' + profile.getName());
+        console.log('Image URL: ' + profile.getImageUrl());
+        console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+    }
+
+    function signOut() {
+        var auth2 = gapi.auth2.getAuthInstance();
+        auth2.signOut().then(function () {
+        console.log('User signed out.');
+        });
+    }
+
+    // WIP
+    this.GetBlackboardTokenCookie = function () {
+        console.log(_getCookie(_blackboardCookieName));
+    };
 }
